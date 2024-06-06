14146d70ec28a36771d646299a37c60587b7b7de
yasai-nodame <yasaisecret1@gmail.com>
2024-06-06 20:23:41 +0900

    live archive保存サイト作成

---
+++ .history/src/pages/Home/Home_20240606201956.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+import Navbar from '../../components/Navbar/Navbar'
+
+const Home = () => {
+    return (
+    <div className='home'>
+        
+    </div>
+    )
+}
+
+export default Home
