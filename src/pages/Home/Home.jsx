c8b289d1d2518ceb64e43b66c2ca6335c4850fb4
yasai-nodame <yasaisecret1@gmail.com>
2024-06-08 23:15:23 +0900

    背景画像追加

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
+import soccer_background from '../../../public/soccer_background.jpg'
 
 const Home = () => {
     return (
     <div className='home'>
         <Navbar/>
+        <div className='soccer-background'>
+            <img src={soccer_background} alt="" className='banner-img'/>
+        </div> 
+        <div className="overlay"></div> 
+        
     </div>
     )
 }
