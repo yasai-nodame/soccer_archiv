a07aafea3c1144a643ab27883d116ab286da4397
yasai-nodame <yasaisecret1@gmail.com>
2024-06-14 23:29:07 +0900

    進捗

---
+++ .history/src/App_20240614232112.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './pages/Home/Home'
+
+const App = () => {
+  return (
+    <div>
+      <div className="background"></div>
+      <Home/>
+    </div>
+  )
+}
+
+export default App
