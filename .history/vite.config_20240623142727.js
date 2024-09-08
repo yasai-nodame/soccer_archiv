08c9176f0357c61e7bfd8f993ca1b7869e91c9cd
yasai-nodame <yasaisecret1@gmail.com>
2024-09-08 21:18:37 +0900

    firebase サービスアカウント更新

--- .history/vite.config_20240623142727.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import env from 'vite-plugin-env-compatible';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), env({ prefix: "VITE", mountedPath: "process.env"})],
-})
--- .history/vite.config_20240702181615.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv';
-import env from 'vite-plugin-env-compatible'; //process.envを使えるように npm i vite-plugin-env-compatible でインストール
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), env({ prefix: "VITE", mountedPath: "process.env"})], //envの設定で process.envを扱えるようにする。
-})
